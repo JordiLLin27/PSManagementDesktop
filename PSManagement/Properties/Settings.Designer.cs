700a4912854ca4a80bff6a2f15c2104e20d57cea
Jordi <jorge.llinares@iesdoctorbalmis.com>
2020-05-08 19:33:47 +0200

    Variable de usuario RutaFacturasDefault valor por defecto

--- PSManagement/Properties/Settings.Designer.cs
+++ PSManagement/Properties/Settings.Designer.cs
@@ -77,7 +77,7 @@ namespace PSManagement.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("%userprofile%")]
         public string RutaFacturasDefault {
             get {
                 return ((string)(this["RutaFacturasDefault"]));
