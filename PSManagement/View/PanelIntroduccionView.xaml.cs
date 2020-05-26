a9c3647e06ac4555919038e8e689d02721960117
Jordi LLinares <jorge.llinares@iesdoctorbalmis.com>
2020-05-26 20:10:33 +0200

    Excepciones controladas para el manual...

--- PSManagement/View/PanelIntroduccionView.xaml.cs
+++ PSManagement/View/PanelIntroduccionView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PSManagement.ViewModel;
+using System.Windows;
 using System.Windows.Controls;
 
 
@@ -17,17 +18,42 @@ namespace PSManagement.View
 
         private void HelpVentasCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
-            (DataContext as PanelIntroduccionVM).HelpVentas();
+            try
+            {
+                (DataContext as PanelIntroduccionVM).HelpVentas();
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("No se ha podido abrir el manual por la sección de ventas", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
         }
 
         private void HelpGestionCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
-            (DataContext as PanelIntroduccionVM).HelpGestion();
+            try
+            {
+                (DataContext as PanelIntroduccionVM).HelpGestion();
+            }
+            catch (System.Exception)
+            {
+                MessageBox.Show("No se ha podido abrir el manual por la sección de gestión", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
         }
 
         private void HelpInventariosCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
-            (DataContext as PanelIntroduccionVM).HelpInventarios();
+            try
+            {
+                (DataContext as PanelIntroduccionVM).HelpInventarios();
+
+            }
+            catch (System.Exception)
+            {
+
+                MessageBox.Show("No se ha podido abrir el manual por la sección de inventarios", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
